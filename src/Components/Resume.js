43d5b3aab9249a09f1ff92e2183cd49cc17db8c9
unknown <Shikder-Vai>
2022-06-09 20:38:12 +0600

    first commit

---
+++ src/Components/Resume.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <div className="">
+      <div className="mt-14">
+        <h1>This is Resume</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
